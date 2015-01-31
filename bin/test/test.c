a1b9666127b11c4f26f6f5a42419b1751674a205
Muthukumar Suresh <muthukumar.suresh@stonybrook.edu>
2015-01-31 17:45:21 -0500

    git commit -a

--- bin/test/test.c
+++ bin/test/test.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
+#define NULL ((void *)0)
 int main(int argc, char *argv[], char *envp[]){
-	
-	execve("/bin/ls", NULL, NULL);	
-	printf("error");	
+	int status;	
+	pid_t child;
+	//int death;
+	char buffer[200];
+	while(1){
+		printf(">>");
+		scanf("%s",buffer);
+		child=fork();	
+		if(child>=0){
+			if(child==0){
+				//printf("child process\n");
+				execve(buffer,NULL,NULL);
+				//printf("error\n");
+			}
+			else {
+				//printf("parent process started\n");
+				waitpid(-1,&status,0);
+				//printf("child with ID %d done\n",death);
+				//printf("parent done\n");	
+			}
+		}
+		else{
+			printf("internal error unsuccessful fork");
+			exit(0);
+		}
+	}
 	return 0;
 
 }
