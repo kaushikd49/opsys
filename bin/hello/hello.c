c86da78a609e3119defc098cb97afad07a69fc8c
Muthukumar Suresh <muthukumar.suresh@stonybrook.edu>
2015-02-01 17:33:08 -0500

    back to 2:40 changes

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+#define NULL ((void *)0)
 int main(int argc, char* argv[], char* envp[]) {
+	int i=0;
 	printf("Hello World!\n");
+	while(envp[i]!=NULL){
+		printf("%d - %s\n",i,envp[i]);
+		i++;
+	}
 	return 0;
 }
