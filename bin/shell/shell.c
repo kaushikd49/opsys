5537864e94df903ef2bd12a5c07e9131a63d4297
Kaushik D <kaushikd49@gmail.com>
2015-02-02 15:41:08 -0500

    space containing argument parsing done. Need to handle escaping of backslash

--- bin/shell/shell.c
+++ bin/shell/shell.c
@@ -11,6 +11,11 @@
 //Execute pipelines of binaries ( /bin/ls | /bin/grep test )
 //Set and use PATH and PS1 variables
 
+// Returns num of delims just based on their presence.
+// Not escaping aware, which is fine for now as this method
+// is used only to find the size of char ** tokens[] and the
+// value returned will always be >= num of tokens actually
+// generated.
 int num_delims(char *str, char delim) {
 	int i = 0, delimCount = 0;
 	while (str[i] != '\0') {
@@ -26,6 +31,7 @@ char **tokenize(char *input, char delim) {
 	char **tokens = (char**) malloc((numDelims + 2) * sizeof(char*));
 	int i = 0, j = 0;
 	while (input[i] != '\0') {
+		// First char itself is a possible token's start location
 		if (i == 0 && input[i] != delim && input[i] != '\0') {
 			tokens[j++] = input;
 		}
@@ -39,6 +45,37 @@ char **tokenize(char *input, char delim) {
 	return tokens;
 }
 
+char * escape_aware_copy(char *input, int startIdx, int endIdx, char delim) {
+	char *token = (char*) malloc((endIdx - startIdx) * sizeof(char));
+	int i = startIdx, j = 0;
+	while (i < endIdx) {
+		if (input[i] == '\\') {
+			if ((i + 1) < endIdx && input[i + 1] == delim) {
+				// escaped delim copy
+				token[j++] = input[i + 1];
+				i += 2;
+				continue;
+			} else {
+				token[j++] = input[i];
+			}
+		} else {
+			token[j++] = input[i];
+		}
+		i++;
+	}
+	return token;
+}
+
+void captureToken(char **tokens, char *input, int *endIdx, int *startIdx,
+		char delim, int i, int *tokenIdx) {
+	*endIdx = i;
+	char * token = escape_aware_copy(input, *startIdx, *endIdx, delim);
+	tokens[*tokenIdx] = token;
+	*tokenIdx = *tokenIdx + 1;
+	*startIdx = -1;
+	*endIdx = -1;
+}
+
 void take_action(char ** tokens) {
 	int i = 0;
 	printf("received tokens\n");
@@ -48,16 +85,55 @@ void take_action(char ** tokens) {
 	}
 }
 
+char **advance_tokenize(char *input, char delim, char fieldEncloser) {
+	int numDelims = num_delims(input, delim);
+	char **tokens = (char**) malloc((numDelims + 2) * sizeof(char*));
+	int i = 0, j = 0, startIdx = -1, endIdx = -1, fieldEnclStarted = 0;
+	while (input[i] != '\0') {
+		if (input[i] != delim) {
+			if (input[i] == '\\' && input[i + 1] == fieldEncloser) {
+				// handle backslash quote (\")
+				if (startIdx == -1) {
+					startIdx = i + 1;
+				}
+				i += 2;
+				continue;
+			} else if (input[i] != fieldEncloser && startIdx == -1) {
+				startIdx = i;
+			} else if (input[i] == fieldEncloser) {
+				if (startIdx == -1) {
+					// token opens with fieldEncloser
+					startIdx = i + 1;
+					fieldEnclStarted = 1;
+				} else {
+					// token opens with fieldEncloser
+					captureToken(tokens, input, &endIdx, &startIdx, delim, i,
+							&j);
+					fieldEnclStarted = 0;
+				}
+			}
+		} else if (fieldEnclStarted != 1 && startIdx > -1) {
+			captureToken(tokens, input, &endIdx, &startIdx, delim, i, &j);
+		}
+		i++;
+	}
+	if (startIdx > -1)
+		captureToken(tokens, input, &endIdx, &startIdx, delim, i, &j);
+	tokens[j] = NULL; 	//sentinel
+	return tokens;
+}
+
 int main(int argc, char* argv[], char* envp[]) {
 
 	char input[ARG_LIMIT];
 	while (1) {
+		printf("shellzhere>> ");
 		scanf(" %1000[^\n]", input);
 //		printf("input is %s ", input);
 		if (strcmp(input, "exit") == 0) {
 			break;
 		}
-		char ** tokens = tokenize(input, ' ');
+		char ** tokens = advance_tokenize(input, ' ', '"');
 		take_action(tokens);
 		free(tokens);
 	}
