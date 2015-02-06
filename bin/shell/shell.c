b92ca5c62f88e9571deb92f875fa720717e2e7cc
Muthukumar Suresh <muthukumar.suresh@stonybrook.edu>
2015-02-06 12:38:22 -0500

    bug fixes exit, getenv setenv

--- bin/shell/shell.c
+++ bin/shell/shell.c
@@ -26,7 +26,7 @@ void handleChildExec(char **tokens, char** envpp) {
 					current = next + 1;
 				} else {
 					printf("command not found\n");
-					break;
+					exit(0);
 				}
 			}
 		} else {
@@ -40,20 +40,56 @@ void handleChildExec(char **tokens, char** envpp) {
 
 void handleGetenv(char **tokens, char *envpp[]) {
 	char *cmd = tokens[1];
+	if(cmd == NULL){
+		printEnviron(envpp);
+		return;
+	}
+	if(tokens[2]!=NULL){
+		printf("too many arguments to getenv\n");
+		return;
+	}
 	char* getenvOutput = getEnv(cmd, envpp);
+	if(getenvOutput[0] != '='){
+		printf("\n");
+		return;
+	}
 	if (getenvOutput != NULL)
 		printf("%s\n", getenvOutput + 1);
 	else
 		printf("\n");
 }
-
+/*
+ * setenv: TESTED
+ * more checks done:
+ * if first only setenv is the command prints all the envvars
+ * if one arg then the env variable is set to blank
+ * if 2 deals with normally
+ * solved the seg fault problem
+ * Similar for getenv
+ * NOT KNOWN: why does top= come in envpp, just run setenv <no arguments> and see!!
+ */
 char ** handleSetenv(char **tokens, char *envpp[]) {
-	char *cmd = tokens[1], *envStr = tokens[2]; //todo validations
-	int size = strlen(cmd) + strlen(envStr) + 2;
+	int envStrlen = 0,cmdlen = 0;
+	char *envStr = NULL, *cmd = NULL;
+	if(tokens[1]!=NULL){
+		cmd = tokens[1];
+		cmdlen = strlen(cmd);
+	}
+	else{
+		printEnviron(envpp);
+		return envpp;
+	}
+
+	if(tokens[2]!=NULL){
+		envStr = tokens[2];
+		envStrlen = strlen(envStr);
+	}
+	int size = cmdlen + envStrlen + 2;
 	char* newStr = (char*) malloc(size * sizeof(char));
 	strcpy(newStr, cmd);
 	strcat(newStr, "=");
-	strcat(newStr, envStr);
+	if(envStr !=NULL)
+		strcat(newStr, envStr);
 	envpp = setEnv(newStr, envpp);
 	return envpp;
 }
@@ -72,24 +108,37 @@ char ** take_action(char** tokens, char *envpp[]) {
 	}
 	return envpp;
 }
-
+/*
+ * tested file handling when no file as such
+ * when I pass multiple files, only first script is run
+ * not checking comments in between coz its not needed.
+ * NOT TESTED: proper scripts with pipe etc,
+ * NOT TESTED: the individual functions it calls, check
+ * function def for details of those tests.
+ */
 char** cmd_line_arg_case(char input[ARG_LIMIT], char* argv[], char* envpp[]) {
 	int flag = 1;
 	//todo: check for size greater than ARG_LIMIT
 	int fileHandle = open(argv[1], O_RDONLY);
+	if (fileHandle < 0){
+		printf("Opening Script: Failed, No such Script exists\n");
+		return envpp;
+	}
 	do {
 		flag = read_line(input, fileHandle);
 		char** tokens = advance_tokenize(input, ' ', '"');
 		if (tokens[0] == NULL || strncmp(tokens[0], "#", 1) == 0)
-			//todo: check for comments in the middle
 			continue;
-
 		envpp = take_action(tokens, envpp);
 	} while (flag == 1);
 	close(fileHandle);
+	
 	return envpp;
 }
-
+/*
+ * FIXED BUG: setenv without arguments setenv with 1 argument.
+ *
+ */
 char** interactive_case(char input[ARG_LIMIT], char* envpp[]) {
 	char ps1[] = "PS1=prompt>>";
 	envpp = setEnv(ps1, envpp);
@@ -106,8 +155,9 @@ char** interactive_case(char input[ARG_LIMIT], char* envpp[]) {
 	return envpp;
 }
 
-char ** process_main(char input[ARG_LIMIT], char* argv[], char* envpp[]) {
-	if (argv[1] != NULL) {
+char ** process_main(int argc, char* argv[], char* envpp[]) {
+	char input[ARG_LIMIT];
+	if (argc >= 2) {   //note it will run only the first script if it has more than one script as parameter. like normal shell
 		envpp = cmd_line_arg_case(input, argv, envpp);
 	} else {
 		envpp = interactive_case(input, envpp);
@@ -123,7 +173,6 @@ char ** process_main(char input[ARG_LIMIT], char* argv[], char* envpp[]) {
 
 //todo : whitespave checks
 int main(int argc, char* argv[], char* envpp[]) {
-	char input[ARG_LIMIT];
-	envpp = process_main(input, argv, envpp);
-	return 0;
+	envpp = process_main(argc, argv, envpp); //made input inside the process_main
+	return 0;								//and checking if it is script with 1 script only
 }
