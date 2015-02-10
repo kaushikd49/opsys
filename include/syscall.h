817234a6e6a767e439518fdd92af46809cef5b6b
Kaushik D <kaushikd49@gmail.com>
2015-02-09 21:16:09 -0500

    formatting

--- include/syscall.h
+++ include/syscall.h
@@ -6,13 +6,13 @@
 typedef uint64_t size_t;
 //typedef int32_t pid_t;
 /*
-static __inline uint64_t syscall_n(int formatc,format,...){
-	const char *format;
-	int i=0;
-	for(for i=0; i<formatc; i++){
+ static __inline uint64_t syscall_n(int formatc,format,...){
+ const char *format;
+ int i=0;
+ for(for i=0; i<formatc; i++){
 
-	}
-}*/
+ }
+ }*/
 static __inline uint64_t syscall_0(uint64_t n) {
 	uint64_t result;
 	__asm__ __volatile(
@@ -21,27 +21,27 @@ static __inline uint64_t syscall_0(uint64_t n) {
 			:"=a"(result)
 			:"0"(n)
 			:"rbx"
-			);
+	);
 	return result;
 }
 
 static __inline uint64_t syscall_1_p(uint64_t n, uint64_t size) {
 	uint64_t result;
 	__asm__ __volatile(
-		//"xor %%rbx, %%rbx\n\t"
-		"syscall"
-		:"=a"(result)
-		:"0"(n),"D"(size));
+			//"xor %%rbx, %%rbx\n\t"
+			"syscall"
+			:"=a"(result)
+			:"0"(n),"D"(size));
 	return result;
 }
 static __inline uint64_t syscall_1(uint64_t n, uint64_t a1) {
 	uint64_t result;
 	__asm__ __volatile(
-				//"movq %0, %%rax\n\t"
-				//"movq %1, %%rdi\n\t"
-				"syscall"\
-				:"=&a" (result)\
-				:"0"(n),"D"(a1));
+			//"movq %0, %%rax\n\t"
+			//"movq %1, %%rdi\n\t"
+			"syscall"
+			:"=&a" (result)
+			:"0"(n),"D"(a1));
 	return result;
 }
 
@@ -49,29 +49,31 @@ static __inline uint64_t syscall_2(uint64_t n, uint64_t a1, uint64_t a2) {
 	return 0;
 }
 
-static __inline uint64_t syscall_3(uint64_t n, uint64_t a1, uint64_t a2, uint64_t a3) {
+static __inline uint64_t syscall_3(uint64_t n, uint64_t a1, uint64_t a2,
+		uint64_t a3) {
 	uint64_t result;
 	__asm__ __volatile__(
-				"syscall"
-				:"=a" (result)\
-				:"0"(n), "D"(a1), "S"(a2), "d"(a3));
+			"syscall"
+			:"=a" (result)
+			:"0"(n), "D"(a1), "S"(a2), "d"(a3));
 	return result;
 }
 static __inline pid_t syscall_4_wait(uint64_t n, uint64_t a1, int *a2, int a3) {
 	uint64_t result;
 	__asm__ __volatile__(
-				"movq $0,%%rcx\n\t"
-				"syscall"
-				:"=a" (result)\
-				:"0"(n), "D"(a1), "S"(a2), "d"(a3));
+			"movq $0,%%rcx\n\t"
+			"syscall"
+			:"=a" (result)
+			:"0"(n), "D"(a1), "S"(a2), "d"(a3));
 	return result;
 }
-static __inline size_t syscall_4_write(uint64_t n,int a1, const void *a2, size_t a3){
+static __inline size_t syscall_4_write(uint64_t n, int a1, const void *a2,
+		size_t a3) {
 	uint64_t result;
 	__asm__ __volatile__ (
-				"syscall"
-				:"=a" (result)
-				:"0"(n), "D"(a1),"S"(a2),"d"(a3));
+			"syscall"
+			:"=a" (result)
+			:"0"(n), "D"(a1),"S"(a2),"d"(a3));
 	return result;
 }
 #endif
